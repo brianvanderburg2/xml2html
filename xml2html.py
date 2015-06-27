69c6116e592b8392deaa6bf78e8801ce202f31d5
Brian Allen Vanderburg II <brianvanderburg@users.sourceforge.net>
2015-06-27 10:51:58 -0400

    Added state tracking support

--- xml2html.py
+++ xml2html.py
@@ -175,7 +175,7 @@ def build():
     for i in params:
         params[i] = etree.XSLT.strparam(params[i])
 
-    #build the output
+    # build the output
     result = transform(inxml, **params)
     if result is None:
         raise Error('No output')
@@ -184,6 +184,33 @@ def build():
 
     # save the output
     open(cmdline.output, 'wb').write(result.encode(cmdline.encoding))
+
+    # done if there is no state
+    if cmdline.state is None:
+        return
+
+    # read state from the file
+    title = inxml.xpath(cmdline.title_xpath, namespaces=cmdline.namespaces) if cmdline.title_xpath else None
+    summary = inxml.xpath(cmdline.summary_xpath, namespaces=cmdline.namespaces) if cmdline.summary_xpath else None
+    year = inxml.xpath(cmdline.year_xpath, namespaces=cmdline.namespaces) if cmdline.year_xpath else None
+    month = inxml.xpath(cmdline.month_xpath, namespaces=cmdline.namespaces) if cmdline.month_xpath else None
+    day = inxml.xpath(cmdline.day_xpath, namespaces=cmdline.namespaces) if cmdline.day_xpath else None
+
+    if not (title and year and month and day):
+        return
+
+    # sanitize
+    relpath = cmdline.params['relinput']
+    title = ' '.join(title)
+    summary = ' '.join(summary) if summary else None
+    year = year[0]
+    month = month[0]
+    day = day[0]
+
+    # update the state staging
+    state = State(cmdline.state)
+    state.staging[relpath] = (relpath, title, summary, year, month, day)
+    state.save(cmdline.state)
 
 
 def cleanup_helper(mo):
@@ -254,6 +281,103 @@ def cleanup(output):
 
     return output
 
+
+# State related
+################################################################################
+
+class State(object):
+    """ State of the files. """
+
+    XML_NAMESPACE = 'urn:mrbavii:xml2html.state'
+
+    def __init__(self, filename=None):
+        # Format is (relpath, title, summary, year, month, day)
+        # entrie is not stored if any item is missing except summary
+        self.entries = {}
+        self.staging = {}
+
+        if filename is None:
+            return
+
+        # Parse the document
+        xml = etree.parse(filename)
+        xml.xinclude()
+
+        root = xml.getroot()
+
+        # get all entires
+        entries = root.findall('{{{0}}}entry'.format(self.XML_NAMESPACE))
+        for i in entries:
+            entry = self.load_entry(i)
+            self.entries[entry[0]] = entry
+
+        # get all staging items
+        entries = root.findall('{{{0}}}staging'.format(self.XML_NAMESPACE))
+        for i in entries:
+            entry = self.load_entry(i)
+            self.staging[entry[0]] = entry
+
+    def load_entry(self, xml):
+        return (
+            xml.get('relpath'),
+            xml.get('title'),
+            xml.get('summary'),
+            xml.get('year'),
+            xml.get('month'),
+            xml.get('day')
+        )
+
+    def save(self, filename):
+        root = etree.Element('{{{0}}}state'.format(self.XML_NAMESPACE))
+
+        for i in self.entries:
+            self.save_entry(root, '{{{0}}}entry'.format(self.XML_NAMESPACE), self.entries[i])
+
+        for i in self.staging:
+            self.save_entry(root, '{{{0}}}staging'.format(self.XML_NAMESPACE), self.staging[i])
+
+        tree = etree.ElementTree(root)
+        tree.write(filename, encoding=cmdline.encoding, xml_declaration=True, method='xml', pretty_print=True)
+
+    def save_entry(self, xml, tag, entry):
+        element = etree.SubElement(xml, tag)
+        element.set('relpath', entry[0])
+        element.set('title', entry[1])
+        if entry[2]:
+            element.set('summary', entry[2])
+        element.set('year', entry[3])
+        element.set('month', entry[4])
+        element.set('day', entry[5])
+
+
+def initialize():
+    """ Initialize the state. """
+    if os.path.isfile(cmdline.state):
+        state = State(cmdline.state)
+    else:
+        state = State()
+
+    state.staging = {}
+    state.save(cmdline.state)
+
+def finalize():
+    state = State(cmdline.state)
+
+    # Move staging to main area
+    for i in state.staging:
+        state.entries[i] = state.staging[i]
+    state.staging = {}
+
+    # Remove missing entires
+    for i in state.entries.keys():
+        fullpath = os.path.join(cmdline.root, i)
+        if not os.path.isfile(fullpath):
+            del state.entries[i]
+
+    # Save the result
+    state.save(cmdline.state)
+
+
 # Entry point
 ################################################################################
 
@@ -284,67 +408,116 @@ def parse_cmdline():
     # Setup and parse command line
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('--help', action='help')
-    parser.add_argument('-r', '--root', dest='root', action='store', required=True, help='root directory')
-    parser.add_argument('-i', '--input', dest='input', action='store', required=True, help='input file name')
-    parser.add_argument('-o', '--output', dest='output', action='store', required=True, help='output file name')
-    parser.add_argument('-t', '--transform', dest='transform', action='store', required=True, help='XSL file name')
-    parser.add_argument('-h', '--header', dest='header', action='store', help='header file name')
-    parser.add_argument('-f', '--footer', dest='footer', action='store', help='footer file name')
-    parser.add_argument('-p', '--property', dest='property', action='append', help='a property name=value pair')
-    parser.add_argument('-e', '--encoding', dest='encoding', action='store', default='utf-8', help='output character encoding')
-    parser.add_argument('-s', '--strip', dest='strip', action='store_true', default=False, help='strip leading spaces from output')
-    parser.add_argument('--selfclose', dest='selfclose_tags', action='append', help='allowed self closing tags')
-    parser.add_argument('--preserve', dest='preserve_tags', action='append', help='preserved tags')
-    parser.add_argument('params', action='store', nargs='*', help='XSL name=value parameters')
+
+    subparsers = parser.add_subparsers(dest='command', help='commands')
+
+    init_parser = subparsers.add_parser('initialize', add_help=False)
+    init_parser.add_argument('--help', action='help')
+    init_parser.add_argument('--state', dest='state', action='store', required=True, help='state file')
+    init_parser.add_argument('--encoding', dest='encoding', action='store', default='utf-8', help='output character encoding')
+
+    build_parser = subparsers.add_parser('build', add_help=False)
+    build_parser.add_argument('--help', action='help')
+    build_parser.add_argument('--root', dest='root', action='store', required=True, help='root directory')
+    build_parser.add_argument('--input', dest='input', action='store', required=True, help='input file name')
+    build_parser.add_argument('--output', dest='output', action='store', required=True, help='output file name')
+    build_parser.add_argument('--transform', dest='transform', action='store', required=True, help='XSL file name')
+    build_parser.add_argument('--header', dest='header', action='store', help='header file name')
+    build_parser.add_argument('--footer', dest='footer', action='store', help='footer file name')
+    build_parser.add_argument('--property', dest='property', action='append', help='a property name=value pair')
+    build_parser.add_argument('--encoding', dest='encoding', action='store', default='utf-8', help='output character encoding')
+    build_parser.add_argument('--strip', dest='strip', action='store_true', default=False, help='strip leading spaces from output')
+    build_parser.add_argument('--selfclose', dest='selfclose_tags', action='append', help='allowed self closing tags')
+    build_parser.add_argument('--preserve', dest='preserve_tags', action='append', help='preserved tags')
+    build_parser.add_argument('--namespace', dest='namespaces', action='append', help='a prefix=namespace pair')
+    build_parser.add_argument('--state', dest='state', action='store', help='state file')
+    build_parser.add_argument('--title-xpath', dest='title_xpath', action='store', help='XPath to extract a title')
+    build_parser.add_argument('--summary-xpath', dest='summary_xpath', action='store', help='XPath to extract a summary')
+    build_parser.add_argument('--year-xpath', dest='year_xpath', action='store', help='XPath to extract a year')
+    build_parser.add_argument('--month-xpath', dest='month_xpath', action='store', help='XPath to extract a month')
+    build_parser.add_argument('--day-xpath', dest='day_xpath', action='store', help='XPath to extract a day')
+    build_parser.add_argument('params', action='store', nargs='*', help='XSL name=value parameters')
+
+    finalize_parser = subparsers.add_parser('finalize', add_help=False)
+    finalize_parser.add_argument('--help', action='help')
+    finalize_parser.add_argument('--root', dest='root', action='store', required=True, help='root directory')
+    finalize_parser.add_argument('--state', dest='state', action='store', required=True, help='state file')
+    finalize_parser.add_argument('--encoding', dest='encoding', action='store', default='utf-8', help='output character encoding')
 
     result = parser.parse_args()
 
     # Set the variables in the options object
     o = Options()
+    o.command = result.command
 
-    o.root = os.path.abspath(result.root)
-    o.input = os.path.abspath(result.input)
-    o.output = os.path.abspath(result.output)
-    o.transform = os.path.abspath(result.transform)
-    o.header = os.path.abspath(result.header) if result.header else None
-    o.footer = os.path.abspath(result.footer) if result.footer else None
+    if o.command == 'initialize':
+        o.state = os.path.abspath(result.state)
+        o.encoding = result.encoding
 
-    o.properties = {}
-    if result.property:
-        for i in result.property:
-            pair = i.split('=', 1)
-            if len(pair) == 2:
-                o.properties[pair[0]] = pair[1]
+    elif o.command == 'build':
+        o.root = os.path.abspath(result.root)
+        o.input = os.path.abspath(result.input)
+        o.output = os.path.abspath(result.output)
+        o.transform = os.path.abspath(result.transform)
+        o.header = os.path.abspath(result.header) if result.header else None
+        o.footer = os.path.abspath(result.footer) if result.footer else None
 
-    o.encoding = result.encoding
-    o.strip = result.strip
+        o.properties = {}
+        if result.property:
+            for i in result.property:
+                pair = i.split('=', 1)
+                if len(pair) == 2:
+                    o.properties[pair[0]] = pair[1]
 
-    o.selfclose_tags = set(DEFAULT_SELFCLOSE_TAGS)
-    if result.selfclose_tags:
-        for i in result.selfclose_tags:
-            update_set(o.selfclose_tags, i)
+        o.encoding = result.encoding
+        o.strip = result.strip
 
-    o.preserve_tags = set(DEFAULT_PRESERVE_TAGS)
-    if result.preserve_tags:
-        for i in result.preserve_tags:
-            update_set(o.preserve_tags, i)
+        o.selfclose_tags = set(DEFAULT_SELFCLOSE_TAGS)
+        if result.selfclose_tags:
+            for i in result.selfclose_tags:
+                update_set(o.selfclose_tags, i)
 
-    relinput = os.path.relpath(o.input, o.root)
-    reloutput = os.path.relpath(o.output, o.root)
-    o.params = {
-        'root': o.root.replace(os.sep, '/'),
-        'input': o.input.replace(os.sep, '/'),
-        'output': o.output.replace(os.sep, '/'),
-        'inputrelroot': '../' * relinput.count(os.sep),
-        'outputrelroot': '../' * reloutput.count(os.sep),
-        'relinput': relinput.replace(os.sep, '/'),
-        'reloutput': reloutput.replace(os.sep, '/')
-    }
-    if result.params:
-        for i in result.params:
-            pair = i.split('=', 1)
-            if len(pair) == 2:
-                o.params[pair[0]] = pair[1]
+        o.preserve_tags = set(DEFAULT_PRESERVE_TAGS)
+        if result.preserve_tags:
+            for i in result.preserve_tags:
+                update_set(o.preserve_tags, i)
+
+        relinput = os.path.relpath(o.input, o.root)
+        reloutput = os.path.relpath(o.output, o.root)
+        o.params = {
+            'root': o.root.replace(os.sep, '/'),
+            'input': o.input.replace(os.sep, '/'),
+            'output': o.output.replace(os.sep, '/'),
+            'inputrelroot': '../' * relinput.count(os.sep),
+            'outputrelroot': '../' * reloutput.count(os.sep),
+            'relinput': relinput.replace(os.sep, '/'),
+            'reloutput': reloutput.replace(os.sep, '/')
+        }
+        if result.params:
+            for i in result.params:
+                pair = i.split('=', 1)
+                if len(pair) == 2 and not pair[0] in o.params:
+                    o.params[pair[0]] = pair[1]
+
+        o.namespaces = {}
+        if result.namespaces:
+            for i in result.namespaces:
+                pair = i.split('=', 1)
+                if len(pair) == 2 and not pair[0] in o.namespaces:
+                    o.namespaces[pair[0]] = pair[1]
+
+        o.state = os.path.abspath(result.state) if result.state else None
+        o.title_xpath = result.title_xpath
+        o.summary_xpath = result.summary_xpath
+        o.year_xpath = result.year_xpath
+        o.month_xpath = result.month_xpath
+        o.day_xpath = result.day_xpath
+
+    elif o.command == 'finalize':
+        o.root = os.path.abspath(result.root)
+        o.state = os.path.abspath(result.state)
+        o.encoding = result.encoding
+
     return o
 
 
@@ -355,7 +528,14 @@ def main():
     try:
         lxml_setup()
         cmdline = parse_cmdline()
-        build()
+        if cmdline.command == 'initialize':
+            initialize()
+        elif cmdline.command == 'build':
+            build()
+        elif cmdline.command == 'finalize':
+            finalize()
+        else:
+            raise Error('Unknown command ' + cmdline.command)
     except (Error, etree.Error, OSError, IOError, ValueError) as e:
         handle_error(e)
 
